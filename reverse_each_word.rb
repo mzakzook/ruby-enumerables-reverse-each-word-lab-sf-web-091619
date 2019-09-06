972d1108382ce9fc309b6bfc633c7527948b5e20
sunny-utility-4827 <mzakzook@me.com>
2019-09-06 22:41:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   array = string.split
-  array = array.each{|element| element.reverse!}
-  array.join(" ")
+  array.each{|element| element.reverse!}.join(" ")
 end
 
 def reverse_each_word(string)
